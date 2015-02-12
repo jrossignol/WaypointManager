62342f542635564fa46cd5e1d9a39b39aa211ab1
Jeremie Rossignol <jrossignol@gmail.com>
2015-02-12 11:50:09 -0700

    2.0.1 - fixed major rendering issue.

--- source/WaypointManager/Properties/AssemblyInfo.cs
+++ source/WaypointManager/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: AssemblyInformationalVersion("2.0.1")]
 [assembly: KSPAssembly("WaypointManager", 1, 0)]
